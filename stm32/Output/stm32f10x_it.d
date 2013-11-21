8a22aad36efe6ad533545ec0342fe1bbcaa1012d
vcheung <sun-be@hotmail.com>
2013-11-21 10:27:53 +0800

    stm32 pro_fixed

--- stm32/Output/stm32f10x_it.d
+++ stm32/Output/stm32f10x_it.d
@@ -1,5 +1,5 @@
 ..\Output\stm32f10x_it.o: stm32f10x_it.c
-..\Output\stm32f10x_it.o: includes.h
+..\Output\stm32f10x_it.o: stm32f10x_it.h
 ..\Output\stm32f10x_it.o: ..\CMSIS\stm32f10x.h
 ..\Output\stm32f10x_it.o: ..\CMSIS\core_cm3.h
 ..\Output\stm32f10x_it.o: C:\Keil\ARM\RV31\INC\stdint.h
@@ -14,6 +14,7 @@
 ..\Output\stm32f10x_it.o: ..\FWlib\inc\stm32f10x_rcc.h
 ..\Output\stm32f10x_it.o: ..\FWlib\inc\stm32f10x_usart.h
 ..\Output\stm32f10x_it.o: ..\FWlib\inc\misc.h
+..\Output\stm32f10x_it.o: includes.h
 ..\Output\stm32f10x_it.o: ..\uCOS-II\Source\ucos_ii.h
 ..\Output\stm32f10x_it.o: ..\APP\app_cfg.h
 ..\Output\stm32f10x_it.o: ..\APP\os_cfg.h
